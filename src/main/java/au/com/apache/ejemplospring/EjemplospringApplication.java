d6010cd75d9c624406808fde19fb8917b047f3c4
Crekold <crekoldalt@gmail.com>
2023-08-31 20:44:51 -0400

    primera version

---
+++ src/main/java/au/com/apache/ejemplospring/EjemplospringApplication.java
@@ -0,0 +1,13 @@
+package au.com.apache.ejemplospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemplospringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjemplospringApplication.class, args);
+	}
+
+}
